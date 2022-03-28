63c7fc7de597f402a1a836fd9ce2ea4320977e65
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-03-28 22:57:31 +0200

    created routing service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { RoutingService } from './services/routing.service';
 
 @Component({
   selector: 'exads-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent {
+
+  constructor(private routingService: RoutingService){
+      this.routingService.navigateToUsersTable();
+  }
+}
